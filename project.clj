15658eaffca3f785292745be8cacd76df8fd85d3
Laurens Van Houtven (lvh) <_@lvh.io>
2019-11-24 13:13:50 -0600

    Tweak flags

--- project.clj
+++ project.clj
@@ -14,5 +14,15 @@
                         "-H:+ReportExceptionStackTraces"
                         "--report-unsupported-elements-at-runtime"
                         "--initialize-at-build-time"
+                        "--allow-incomplete-classpath"
+                        ;; re: --allow-incomplete-classpath: see
+                        ;; https://github.com/martinklepsch/clj-http-lite/commit/3f41fc53a1b692549c88a8602e753cfb887330ae
+                        ;; the original java.xml.bind.DatatypeConverter never
+                        ;; actually gets used on JDK11, but it's still
+                        ;; referenced as part of a backwards compatibility trial
+                        ;; import, which graal complains about. There's a Graal
+                        ;; ticket to make this flag more precise so you don't
+                        ;; have to disable classpath completeness checks
+                        ;; entirely: https://github.com/oracle/graal/issues/1664
                         "--enable-https"]}
   :profiles {:uberjar {:aot :all}})
